42171295bcf71da8035af5074a99d5275f523d25
remon-rakibul <remon.rakibul.star@gmail.com>
2019-08-16 16:39:48 +0600

    updated

--- test-skill-cv-bn.py
+++ test-skill-cv-bn.py
@@ -9,49 +9,51 @@ nlp = spacy.load('skill-cv-bn')
 def create_dic(filename):
     result = {
                 'Filename': filename,
-                'Skills':[],
-                'College Name':[],
-                'Degree':[],
-                'Graduation Year':[],
-                'Years of Experience':[],
-                'Companies worked at':[],
+                'Name':[],
+                'Number':[],
+                'Email':[],
+                'Experience':[],
+                'Companies':[],
                 'Designation':[],
-                'Location':[],
-                'Start time':[],
-                'End time':[],
                 'Responsibilities':[],
-                'Mobile':[],
-                'Achievement':[]
+                'Skills':[],
+                'Certification':[],
+                'Degree':[],
+                'Institution':[],
+                'Edu_subject':[],
+                'Graduation_year':[]
                 }
     return result      
 
 def get_info(text, dic_name):
     nlp_text = nlp(text)
     for e in nlp_text.ents:
-        if e.label_ == 'Skills':
-            dic_name['Skills'].append(e.text)
-        elif e.label_ == 'College Name':
-            dic_name['College Name'].append(e.text)
-        elif e.label_ == 'Degree':
-            dic_name['Degree'].append(e.text)
-        elif e.label_ == 'Graduation Year':
-            dic_name['Graduation Year'].append(e.text)
-        elif e.label_ == 'Years of Experience':
-            dic_name['Years of Experience'].append(e.text)
+        if e.label_ == 'Name':
+            dic_name['Name'].append(e.text)
+        elif e.label_ == 'Number':
+            dic_name['Number'].append(e.text)
+        elif e.label_ == 'Email':
+            dic_name['Email'].append(e.text)
+        elif e.label_ == 'Experience':
+            dic_name['Experience'].append(e.text)
+        elif e.label_ == 'Companies':
+            dic_name['Companies'].append(e.text)
         elif e.label_ == 'Designation':
             dic_name['Designation'].append(e.text)
-        elif e.label_ == 'Location':
-            dic_name['Location'].append(e.text)
-        elif e.label_ == 'Start time':
-            dic_name['Start time'].append(e.text)
-        elif e.label_ == 'End time':
-            dic_name['End time'].append(e.text)
         elif e.label_ == 'Responsibilities':
             dic_name['Responsibilities'].append(e.text)
-        elif e.label_ == 'Mobile':
-            dic_name['Mobile'].append(e.text)
-        elif e.label_ == 'Achievement':
-            dic_name['Achievement'].append(e.text)
+        elif e.label_ == 'Skills':
+            dic_name['Skills'].append(e.text)
+        elif e.label_ == 'Certification':
+            dic_name['Certification'].append(e.text)
+        elif e.label_ == 'Degree':
+            dic_name['Degree'].append(e.text)
+        elif e.label_ == 'Institution':
+            dic_name['Institution'].append(e.text)
+        elif e.label_ == 'Edu_subject':
+            dic_name['Edu_subject'].append(e.text)
+        elif e.label_ == 'Graduation_year':
+            dic_name['Graduation_year'].append(e.text)
 
 def extract_info(cv_dir):
     extracted_info = []
@@ -60,20 +62,20 @@ def extract_info(cv_dir):
         if file.endswith('.pdf'):
             text = convert2txt.extract_text(cv_dir+file, '.pdf')
             result = create_dic(file)
-            get_info(text)
+            get_info(text, result)
             extracted_info.append(result)
         elif file.endswith('.doc'):
             text = convert2txt.extract_text(cv_dir+file, '.doc')
             result = create_dic(file)
-            get_info(text)
+            get_info(text, result)
             extracted_info.append(result)
         elif file.endswith('.docx'):
             text = convert2txt.extract_text(cv_dir+file, '.docx')
             result = create_dic(file)
-            get_info(text)
+            get_info(text, result)
             extracted_info.append(result)
         elif file.endswith('.txt'):
-            with open(cv_dir+file, encoding='utf-8') as f:
+            with open(cv_dir+file, encoding='utf-8-sig') as f:
                 text = f.read()
             result = create_dic(file)
             get_info(text, result)
@@ -82,9 +84,13 @@ def extract_info(cv_dir):
 
 def process(txt):
     data = txt
-    data = re.sub(r'\W+', ' ', data)
     data = re.sub(r'â+', ' ', data)
     data = re.sub(r'ï+', ' ', data)
+    data = re.sub(r'\\u+', ' ', data)
+    data = re.sub(r'â€+', ' ', data)
+    data = re.sub(r'™+', ' ', data)
+    data = re.sub(r'â€+', ' ', data)
+    data = re.sub(r'â+', ' ', data)
     data = re.sub(r'\s+', ' ', data)
     return data
 
@@ -94,25 +100,26 @@ def create_csv_row_dic(headers, dic):
         if i == 0:
             result[headers[i]] = dic[headers[i]]
         else:
-            result[headers[i]] = [process(i) for i in dic[headers[i]]]
+            result[headers[i]] = ', '.join([process(i) for i in dic[headers[i]]])
     return result
 
 def export_to_csv(csv_name, extracted_info):
     with open(csv_name, 'w', newline='', encoding="utf-8") as csv_file:
         headers = [
                 'Filename',
-                'Skills',
-                'College Name',
-                'Degree',
-                'Graduation Year',
-                'Years of Experience',
+                'Name',
+                'Number',
+                'Email',
+                'Experience',
+                'Companies',
                 'Designation',
-                'Location',
-                'Start time',
-                'End time',
                 'Responsibilities',
-                'Mobile',
-                'Achievement'
+                'Skills',
+                'Certification',
+                'Degree',
+                'Institution',
+                'Edu_subject',
+                'Graduation_year'
                 ]
         writer = csv.DictWriter(csv_file, fieldnames=headers)
         writer.writeheader()
@@ -120,4 +127,4 @@ def export_to_csv(csv_name, extracted_info):
             write_dic = create_csv_row_dic(headers, dic)
             writer.writerow(write_dic)
 
-export_to_csv('skills_report.csv', extract_info('cv/txt/'))
+export_to_csv('skills_report.csv', extract_info('../cv/txt/'))
